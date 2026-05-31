402b9687cf72acf3c3ec05c6fee5fb8bc0bd0369
Muhammad Azqilana <mazqulunu@gmail.com>
2026-05-31 12:25:02 +0800

    Update footer link to open in a new tab

--- Layout/footer.js
+++ Layout/footer.js
@@ -5,10 +5,9 @@
 export default function () {
   return `
     <footer>
-      <a href="https://github.com/azqilana/mathnote/releases"
-        id="downloadBtn" class="download-btn">
-        <span class="icon">⬇</span> Download
-      </a>
+  <a href="https://github.com/azqilana/mathnote/releases" 
+   target="_blank"
+   id="downloadBtn" class="download-btn">
       <p id="copyright">&copy; Arjuna Arc</p>
     </footer>
   `
